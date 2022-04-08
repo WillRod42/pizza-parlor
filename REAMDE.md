fdd87e24c8baac04a9f04996e2586a6096a3bae0
William Rodriguez <william@rodriguez1.com>
2022-04-08 11:44:53 -0700

    Add calc price logic for given 'none' inputs

--- REAMDE.md
+++ REAMDE.md
@@ -1,6 +1,6 @@
 Describe: calcPrice()<br>
 <br>
-Test: "If called on object that has missing attributes it should return 0"<br>
+Test: "If called on an order that has missing attributes it should return 0"<br>
 Code:
 ```
 let order = new Order();
@@ -8,3 +8,10 @@ order.calcPrice();
 ```
 Expected-Output: 0
 
+Test: "If given an order that has no cheese, no sauce, thin/normal crust, and no toppings selected and is medium sized to should return 2.5"
+Code:
+```
+let order = new Order({"kind": "none", "amount": "none"}, {"kind": "none", "amount": "none"}, "thin", [{"kind": "none", "amount": "none"}], "medium");
+order.calcPrice();
+```
+Expected-Output: 2.5
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,57 @@
-function Order() {
-  this.cheese = "";
-  this.sauce = "";
-  this.crust = "";
-  this.toppings = "";
+//Mock Database for pizza parts
+const pizzaData = {
+  "cheese": {"Normal": 1, "Diary-free": 1.5, "None": 0},
+  "sauce": {"Normal": 1, "Marinara": 1, "BBQ": 1.5, "Alfredo": 1.5, "None": 0},
+  "crust": {"Normal": 1, "Thin": 0.5, "Deep Dish": 1.5, "Stuffed": 2},
+  "size": {"Small": 1, "Medium": 2, "Large": 3},
+  "toppings": {
+    "Pepperoni": 1,
+    "Ham": 1,
+    "Beef": 1.5,
+    "Salami": 1.5,
+    "Chicken": 1,
+    "Sausage": 2,
+    "Pineapple": 1.5,
+    "Bell Peppers": 2,
+    "Olives": 1,
+    "Mushrooms": 1,
+    "None": 0
+  }
+};
+
+function Order(cheese, sauce, crust, toppings, size) {
+  this.cheese = cheese;
+  this.sauce = sauce;
+  this.crust = crust;
+  this.toppings = toppings;
+  this.size = size;
 }
 
 Order.prototype.calcPrice = function() {
-  return 0;
+  let price = 0
+  price += calcPartPrice(this.cheese, "cheese");
+  price += calcPartPrice(this.sauce, "sauce");
+  price += pizzaData.size[this.size];
+  price += pizzaData.crust[this.crust];
+  this.toppings.forEach(function(topping) {
+    price += calcPartPrice(topping, "toppings");
+  });
+
+  return price;
 }
+
+function calcPartPrice(part, partName) {
+  let modifier = 0;
+  switch (part.amount) {
+    case ("light"): modifier = 0.5; break;
+    case ("normal"): modifier = 1; break;
+    case ("extra"): modifier = 2; break;
+    deafult: break;
+  }
+
+  return pizzaData[partName][part.kind] * modifier;
+}
+
+
+  let order = new Order({"kind": "None", "amount": "none"}, {"kind": "None", "amount": "none"}, "Thin", [{"kind": "None", "amount": "none"}], "Medium");
+  console.log(order.calcPrice());
